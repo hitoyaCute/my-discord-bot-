3dedf81cb146e53e1f4fb1d189bc9f03584e0e77
hitoyaCute <147114029+hitoyaCute@users.noreply.github.com>
2024-03-06 11:39:35 +0800

    Update __main__.py
    add new features

--- __main__.py
+++ __main__.py
@@ -7,6 +7,7 @@ hitoya = 1200330195387813903
 
 import requests
 import json
+import time
 #import pathlib
 import os
 import discord
@@ -84,21 +85,28 @@ async def on_ready():
 async def on_message(message):
 	global webhook
 	isnotcmndyrig = False
-	#make sure it will ignore message of anny bot
-	if message.author.bot:
-		return
+	
+	sender = message.author
+	
 	guildId = message.guild.id
 	guildName = message.guild.name
-	member = message.author
+	member = sender
 	g = message.guild
 	member.guild = g
+	
 	
 	content = message.content
 	
 	botping = bot.user.mention
+	
+	
+	#make sure it will ignore message of anny bot
+	if sender.bot:
+		return
+	
 	#message base bot trigger her
 	
-	if message.author.id in [hitoya]:
+	if sender.id in [hitoya]:
 		#builder only commands
 		#creates member object
 		
@@ -131,13 +139,45 @@ async def on_message(message):
 	#elif content.startswith()
 	else :
 		isnotcmndyrig = True
-	print("working",webhook,isnotcmndyrig)
 	if webhook and isnotcmndyrig:
 		
-		print(webhook_message(message.author.display_name,content,message.author.display_avatar.url))
+		print(webhook_message(sender.display_name,content,sender.display_avatar.url))
 	
 	
 	
+	if  sender.id not in data["User_data"]:
+		data['User_data'][str(sender.id)] = {"name":sender.name,
+																		  "last_msg_time":0,
+																		  "mgs_spd_wrng_lvl":0,
+																		  "last_time_got_wrng":0}
+	
+	
+	old_time= data['User_data'][str(sender.id)]["last_msg_time"]
+	wrng_lvl = data['User_data'][str(sender.id)]["mgs_spd_wrng_lvl"]
+	last_time_got_wrng = data['User_data'][str(sender.id)]["last_time_got_wrng"]
+	cht_speed = time.time()-old_time
+	
+	#if going too fast
+	if cht_speed >= data["Guild_data"][str(guildId)]["chat_speed"]:
+		out:str=""
+		if wrng_lvl==0:
+			out = sender.mention+"woops going too fast do that again I'll add warning for ya"
+		elif wrng_lvl==1:
+			out = sender.mention+"ur going too fast again"
+		elif wrng_lvl==2:
+			pass
+		
+		data['User_data'][str(sender.id)]["mgs_spd_wrng_lvl"]+=1
+		data['User_data'][str(sender.id)]["last_time_got_wrng"] = time.time()
+		await message.channel.send(out)
+
+	#if not
+	elif time.time()-last_time_got_wrng >= 24*60*60 and wrng_lvl <= 3:
+		data['User_data'][str(sender.id)]["mgs_spd_wrng_lvl"] = 0
+	
+	
+	data['User_data'][str(sender.id)]["last_msg_time"] = time.time()
+	print(f"{time.time()-last_time_got_wrng = }")
 	
 	
 	
@@ -145,7 +185,7 @@ async def on_message(message):
 	#donr touch
 	if not check_server_data(message.guild.id):
 		await message.channel.send("hmmm seems this server is not yet recognize leme load it on my data base")
-		data_save()
+	data_save()
 	#await bot.process_commands(message)
 
 @bot.event
@@ -193,24 +233,35 @@ async def reactEm(ctx):
 	await ctx.reply('nice')
 	await ctx.message.add_reaction("🦊")
 	await ctx.message.add_reaction("🤯")
+	#await ctx.message.add_reaction("<:man_facepalming:>")
+	
+	await ctx.message.add_reaction("<:ayoooo:1214419776366182480>")
 	
 
 #slash base comman
+@bot.tree.command(name="say")
+async def say(interaction: discord.Interaction,recipient:discord.Member,channel:discord.TextChannel,dm:bool=False,message:str="hey {recipient.mention} this is {self.display_name}"):
+	if dm:
+		await recipient.send(message)
+	else:
+		await channel.send(message.format(recipient=recipient, self=interaction.user))
+	await interaction.response.send_message(interaction.user.mention+' message is sent', ephemeral=True)
+
 @bot.tree.command(name="ello")
 async def ello(interaction: discord.Interaction):
 	await interaction.response.send_message('ello '+interaction.user.mention, ephemeral=True)
 
 @bot.tree.command(name="set_chat_speed", description="only for user with \"admin\" role")
-async def wlcm_msg(interaction:discord.Interaction,max_chat_per_sec:float = float("inf")):
+async def set_speed(interaction:discord.Interaction,max_chat_per_min:float = float("inf")):
 	guild = str(interaction.guild.id)
 	
 	if "admin"  not in [str(role).lower() for role in interaction.user.roles]:
 		await interaction.response.send_message("srr but you need to have role \"admin\" in order to use this")
 		return
-	data["Guild_data"][guild]["chat_speed"] = 1.0/max_chat_per_sec
+	data["Guild_data"][guild]["chat_delay"] = 60.0/max_chat_per_min
 	
 	data_save()
-	await interaction.response.send_message(interaction.user.mention+f" new chat speed is set to {max_chat_per_sec} per sec",ephemeral=True)
+	await interaction.response.send_message(interaction.user.mention+f" new chat speed is set to {max_chat_per_min} per sec",ephemeral=True)
 	
 
 @bot.tree.command(name="set_welcome_message", description="only for user with \"admin\" role")
@@ -238,7 +289,7 @@ async def exit_msg(interaction:discord.Interaction,message:str="{member.name} ex
 	await interaction.response.send_message(interaction.user.mention+" new farewell message is set to "+message,ephemeral=True)
 
 @bot.tree.command(name="set_welcome_channel", description="only for user with \"admin\" role")
-async def wlcm_msg(interaction:discord.Interaction,channel:discord.TextChannel):
+async def wlcm_chnl(interaction:discord.Interaction,channel:discord.TextChannel):
 	guild = str(interaction.guild.id)
 	
 	if "admin"  not in [str(role).lower() for role in interaction.user.roles]:
@@ -252,7 +303,7 @@ async def wlcm_msg(interaction:discord.Interaction,channel:discord.TextChannel):
 
 
 @bot.tree.command(name="set_farewell_channel", description="only for user with \"admin\" role")
-async def exit_msg(interaction:discord.Interaction,channel:discord.TextChannel):
+async def exit_chnl(interaction:discord.Interaction,channel:discord.TextChannel):
 	guild = str(interaction.guild.id)
 	
 	if "admin"  not in [str(role).lower() for role in interaction.user.roles]:
